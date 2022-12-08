d48c05b426051c649c4f72b6941d0277940cd21e
秦锋 <iamzhoufei@gmail.com>
2022-12-08 10:35:33 +0800

    test: test vercel deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { getConfig } from './utils';
 
 import { UserModule } from './user/user.module';
 
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
 // ConfigModule 默认会从 项目根路径 载入并解析一个 .env 文件，并将文件内容与 process.env 合并环境变量键值对
 // 并将结果存储到一个可以通过 ConfigService 访问的私有结构
 
@@ -20,7 +23,7 @@ const SharedModule = [
 ];
 @Module({
   imports: [...SharedModule, UserModule],
-  controllers: [],
-  providers: [],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
